43cca74a5d4b4453c3c8611fb8aa71bb3298624d
abdulrahman-awad1 <aulrahman034@gmail.com>
2026-06-13 18:25:15 +0300

    Fix: update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (!class_exists(\Laravel\Pail\PailServiceProvider::class)) {
+            $this->app->getProvider(\Laravel\Pail\PailServiceProvider::class)
+                ? $this->app->make(\Illuminate\Foundation\Application::class)->getProvider(\Laravel\Pail\PailServiceProvider::class)
+                : null;
+        }
     }
 
     /**
